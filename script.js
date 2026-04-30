1a181aba2ce5c3674ab65688015ad51489d88589
santi <santiagobaez2406@gmail.com>
2026-04-30 13:01:50 -0500

    mi primer commit: mapa inicial

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let latitudes = [4.7110];
+    let longitudes = [-74.0721];
+    let nombres = ["Bogotá"];
+
+    Plotly.newPlot("mi-mapa", [{
+        type: "scattermapbox",
+        mode: "markers",
+        lat: latitudes,
+        lon: longitudes,
+        text: nombres,
+        marker: {
+            size: 12,
+            color: "red"
+        }
+    }], {
+        mapbox: {
+            style: "open-street-map",
+            center: {
+                lat: 4.7110,
+                lon: -74.0721
+            },
+            zoom: 12
+        },
+        margin: {t:0,b:0,l:0,r:0}
+    });
+
+    const mapa = document.getElementById("mi-mapa");
+
+    mapa.on('plotly_afterplot', function () {
+
+        const mapboxCanvas = mapa.querySelector(".mapboxgl-canvas");
+
+        mapboxCanvas.addEventListener("click", function(e){
+
+            const graphDiv = document.getElementById("mi-mapa");
+
+            const internalMap = graphDiv._fullLayout.mapbox._subplot.map;
+
+            const rect = mapboxCanvas.getBoundingClientRect();
+
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            const lngLat = internalMap.unproject([x, y]);
+
+            latitudes.push(lngLat.lat);
+            longitudes.push(lngLat.lng);
+            nombres.push("Nuevo Pin");
+
+            Plotly.restyle("mi-mapa", {
+                lat: [latitudes],
+                lon: [longitudes],
+                text: [nombres]
+            });
+
+        });
+
+    });
+
+});
